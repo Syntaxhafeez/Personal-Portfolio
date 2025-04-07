f48e461e982f220615e214545a811c5e76bab6e5
Syntaxhafeez <syntaxhafeez@gmail.com>
2025-04-07 14:54:45 +0530

    Minor Changes

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -20,8 +20,8 @@ const Computers = ({ isMobile }) => {
       <pointLight intensity={1} />
       <primitive
         object={computer.scene}
-        scale={isMobile ? 0.45 : 0.75}
-        position={isMobile ? [0, -3.5, -1.8] : [0, -3.25, -1.5]}
+        scale={isMobile ? 0.6 : 0.75}
+        position={isMobile ? [0, -3, -2] : [0, -3.25, -1.5]}
         rotation={[-0.01, -0.2, -0.1]}
       />
     </mesh>
@@ -51,10 +51,7 @@ const ComputersCanvas = () => {
       frameloop="demand"
       shadows
       dpr={[1, 2]}
-      camera={{
-        position: isMobile ? [10, 2, 3.5] : [20, 3, 5],
-        fov: 25,
-      }}
+      camera={{ position: [20, 3, 5], fov: 25 }}
       gl={{ preserveDrawingBuffer: true }}
     >
       <Suspense fallback={<CanvasLoader />}>
